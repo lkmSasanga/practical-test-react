062075b698783d57e11a5ab6914aaeab0e2a0e3c
MalinduSasanga <lkmsasanga@gmail.com>
2022-01-01 11:10:34 +0530

    data passed to card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Spinner from "../src/components/UI/Spinner/Spinner";
+import Card from "../src/components/ItemCard/ItemCard";
+import axios from "axios";
 
 function App() {
+  const [shipmentDetails, setShipmentDetails] = useState("");
+  const [showSpinner, setShowSpinner] = useState(false);
+
+  useEffect(() => {
+    setShowSpinner(true);
+
+    axios
+      .get(
+        "https://my-json-server.typicode.com/prasadhewage/ecommerce/shipments"
+      )
+      .then(response => {
+        console.log(response.data);
+        console.log(response.status);
+        if (response.status === 200) {
+          console.log("success");
+          setShowSpinner(false);
+          setShipmentDetails(response.data);
+        }
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+
+      {shipmentDetails && (
+        <div>
+          {shipmentDetails.map(shipment => (
+            <div>
+              <Card key={shipment._id}>
+                <div>
+                  <p>{shipment.name}</p>
+                </div>
+              </Card>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {showSpinner && <Spinner />}
     </div>
   );
 }
